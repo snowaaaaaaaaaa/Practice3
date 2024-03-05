3315efac13a971e8c2e393817125504c4f333937
snowaaaaaaaaaa <144525592+snowaaaaaaaaaa@users.noreply.github.com>
2024-03-05 23:15:12 +0100

    Add files via upload

---
+++ View.java
@@ -0,0 +1,11 @@
+package com.mycompany.ex02;
+
+import java.io.IOException;
+
+public interface View {
+    int DEFAULT_NUM = 10;
+
+    public void viewShow();
+
+    public void viewInit();
+}
---
+++ Viewable.java
@@ -0,0 +1,7 @@
+package com.mycompany.ex02;
+
+import java.io.IOException;
+
+public interface Viewable {
+    // your existing implementation here
+}
